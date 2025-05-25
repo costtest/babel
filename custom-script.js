db8d3f9b2a2064e3d23e8b450e5d0ad21560f8b8
costtest <ossamachadliai@gmail.com>
2025-05-25 17:50:33 +0100

    Update custom-script.js
    add js test

--- custom-script.js
+++ custom-script.js
@@ -1,6 +1,5 @@
-// حماية بسيطة بالدومين والمفتاح
 (function() {
-  const ALLOWED_DOMAIN = "client1.youcan.shop";
+  const ALLOWED_DOMAIN = "mizmo.youcan.store";
   const ACCESS_KEY = "BABEL123";
 
   if (window.location.hostname !== ALLOWED_DOMAIN || ACCESS_KEY !== "BABEL123") {
@@ -8,11 +7,8 @@
     return;
   }
 
-  // تحميل CSS من GitHub عبر jsDelivr
   let css = document.createElement("link");
   css.rel = "stylesheet";
   css.href = "https://cdn.jsdelivr.net/gh/costtest/babel/custom-style.css";
   document.head.appendChild(css);
-
-  // يمكن تضيف هنا أي JS إضافي خاص بالتعديلات
 })();
